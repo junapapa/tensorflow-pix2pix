fix batch-normalization

main.py
@@ -36,7 +36,7 @@ flags.DEFINE_boolean("flip", True, "if flip the images for data argumentation [T
 # training parameters
 flags.DEFINE_integer("epoch", 200, "# of epoch [200]")
 flags.DEFINE_integer("batch_size", 1, "# images in batch [1]")
-flags.DEFINE_integer("train_size", np.inf, "# images used to train [np.inf]")
+flags.DEFINE_float("train_size", np.inf, "# images used to train [np.inf]")
 
 # optimizer
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")

model.py
@@ -84,7 +84,7 @@ class Pix2Pix(object):
 
         # sampler
         self.fake_output_sample = generator(self.real_input, self.gen_num_filter, self.output_dim,
-                                            reuse=True, training=True)
+                                            reuse=True, training=False)
 
         # summary (tensorboard)
         self.D_real_summary = tf.summary.histogram("D_real", self.D_real)
@@ -114,8 +114,9 @@ class Pix2Pix(object):
         self.g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
 
         # optimizer
-        self.opt_d = optimizer(loss=self.D_loss, var_list=self.d_vars, learning_rate=learning_rate, beta1=beta1)
-        self.opt_g = optimizer(loss=self.G_loss, var_list=self.g_vars, learning_rate=learning_rate, beta1=beta1)
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            self.opt_d = optimizer(loss=self.D_loss, var_list=self.d_vars, learning_rate=learning_rate, beta1=beta1)
+            self.opt_g = optimizer(loss=self.G_loss, var_list=self.g_vars, learning_rate=learning_rate, beta1=beta1)
 
         # summary (merge)
         self.G_summary = tf.summary.merge([self.D_fake_summary, self.fake_output_summary,

trainer.py
@@ -221,7 +221,7 @@ def run_test(model, testset_dir):
         batch_output = np.array(batch_output).astype(np.float32)
 
         batch_generate = model.sess.run([model.fake_output_sample], feed_dict={model.real_input: batch_input,
-                                                                              model.real_output: batch_output})
+                                                                               model.real_output: batch_output})
 
         # write results
         save_dir = os.path.join(model.test_dir, model.dataset_name)